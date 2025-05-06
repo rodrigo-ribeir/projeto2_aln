6e05a068001bb14b3cb369453b05057050fff8c9
rodrigo-ribeir <rodrigodacruzribeiro10@gmail.com>
2025-05-06 19:05:29 -0300

    Código com condicionamento de A (item b))

---
+++ A_condicionamento.py
@@ -0,0 +1,9 @@
+import numpy as np
+from numpy.linalg import cond
+
+m_values = [5, 10, 20, 50, 100, 1000, 10000, 100000]
+for m in m_values:
+    t = np.linspace(0, 1, m)
+    A = np.array([[m, np.sum(t)], [np.sum(t), np.sum(t**2)]])
+    condition_number = cond(A)
+    print(f"Para m = {m}, o número de condição de A é: {condition_number:.2f}")
